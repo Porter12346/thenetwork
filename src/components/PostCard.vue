c2f21e2661f89db9d62161fee0c3df61ea96419c
Porter Young <porteryoung2005@gmail.com>
2024-07-19 12:28:33 -0600

    you can now go to the profile page

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { format } from 'timeago.js';
 import { Post } from '../models/Post.js';
+import { RouterLink } from 'vue-router';
 
 const props = defineProps({
     postProp: { type: Post, required: true }
@@ -18,10 +19,12 @@ const props = defineProps({
 
     </div>
     <div class="card mb-3">
+        <RouterLink :to="{name: 'Profile', params: {profileId: postProp.creator.id}}">
         <div class="d-flex align-items-center mt-2">
             <img :src="postProp.creator.picture" :alt="postProp.creator.name" class="profile-img mx-2">
             <h2>{{ postProp.creator.name }}</h2>
         </div>
+    </RouterLink>
         <p class="card-text">{{ postProp?.body }}</p>
         <p class="card-text"><small class="text-muted">Posted {{ format(postProp.createdAt) }}</small></p>
         <img v-if="postProp.imgUrl" :src="postProp.imgUrl" class="card-img-top" alt="post Image">
